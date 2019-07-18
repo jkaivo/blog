bc66ce9d01485ef1876374c46b67e20bc83e956a
Jakob Kaivo <jkk@ung.org>
2019-07-18 19:22:18 -0400

    move #defines to header

--- blog.h
+++ blog.h
@@ -4,6 +4,20 @@
 #define PASSWORD_FILE	"/blog/password"
 #define DATA_DIRECTORY	"/jakob"
 
+#define DOCTYPE "<!DOCTYPE html>\n"
+#define HTML    "<html lang=\"en\">\n"
+#define META    "<meta charset=\"utf-8\">\n"
+#define TITLE   "<title>%s</title>\n"
+#define ICON    "<link type=\"shortcut icon\" href=\"/icon.png\">\n"
+#define STYLE   "<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\">\n"
+#define BODY    "</html>\n<body>\n"
+
+#define HTML_HEAD DOCTYPE HTML META TITLE ICON STYLE BODY
+
+#define ADDRESS "<address>Copyright &copy; %d <a href=\"/\">Jakob Kaivo</a> &lt;<a href=\"mailto:jakob@kaivo.net\">jakob@kaivo.net</a>&gt;\n"
+
+#define HTML_TAIL "\n" ADDRESS "</body>\n</html>\n"
+
 void read_post_data(void);
 char *find_post_data(char *key);
 
