2635b799b0493354ec9660258d7f9899d640bac1
Jakob Kaivo <jkk@ung.org>
2020-10-20 15:47:13 -0400

    add RSS feed to header

--- blog.h
+++ blog.h
@@ -5,11 +5,12 @@
 #define HTML    "<html lang=\"en\">\n"
 #define META    "<meta charset=\"utf-8\">\n"
 #define TITLE   "<title>%s</title>\n"
+#define RSS	"<link rel=\"alternate\" type=\"application/rss+xml\" title=\"Feed\" href=\"/feed.xml\">"
 #define ICON    "<link type=\"shortcut icon\" href=\"/icon.png\">\n"
 #define STYLE   "<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\">\n"
 #define BODY    "</head>\n<body>\n"
 
-#define HTML_HEAD DOCTYPE HTML META TITLE ICON STYLE BODY
+#define HTML_HEAD DOCTYPE HTML META TITLE RSS ICON STYLE BODY
 
 #define ADDRESS "<address>Copyright &copy; %1$d <a href=\"/\">%2$s</a> &lt;<a href=\"mailto:%3$s\">%3$s</a>&gt;</address>\n"
 
